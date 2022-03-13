c6ad6cb688d9287d7009a2f39899196bcc89ead9
who-hoo <who.ho3ov@gmail.com>
2022-03-13 22:01:53 +0900

    :white_check_mark: 상품 저장소 테스트 코드 작성

---
+++ src/test/java/study/springitemservice/domain/item/ItemRepositoryTest.java
@@ -0,0 +1,66 @@
+package study.springitemservice.domain.item;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+class ItemRepositoryTest {
+
+    ItemRepository itemRepository = new ItemRepository();
+
+    @AfterEach
+    void tearDown() {
+        itemRepository.clearStore();
+    }
+
+    @Test
+    void save() {
+        // given
+        Item item = new Item("itemA", 10000, 10);
+
+        // when
+        Item savedItem = itemRepository.save(item);
+
+        // then
+        Item findItem = itemRepository.findById(item.getId());
+        assertThat(findItem).isEqualTo(savedItem);
+    }
+
+    @Test
+    void findAll() {
+        // given
+        Item item1 = new Item("item1", 10000, 10);
+        Item item2 = new Item("item2", 20000, 20);
+
+        itemRepository.save(item1);
+        itemRepository.save(item2);
+
+        // when
+        List<Item> result = itemRepository.findAll();
+
+        // then
+        Assertions.assertThat(result.size()).isEqualTo(2);
+        Assertions.assertThat(result).contains(item1, item2);
+    }
+
+    @Test
+    void update() {
+        // given
+        Item item = new Item("item1", 10000, 10);
+        Item savedItem = itemRepository.save(item);
+        Long itemId = savedItem.getId();
+
+        // when
+        Item updateParam = new Item("item2", 20000, 30);
+        itemRepository.update(itemId, updateParam);
+        Item findItem = itemRepository.findById(itemId);
+
+        // then
+        Assertions.assertThat(findItem.getItemName()).isEqualTo(updateParam.getItemName());
+        Assertions.assertThat(findItem.getPrice()).isEqualTo(updateParam.getPrice());
+        Assertions.assertThat(findItem.getQuantity()).isEqualTo(updateParam.getQuantity());
+    }
+}
