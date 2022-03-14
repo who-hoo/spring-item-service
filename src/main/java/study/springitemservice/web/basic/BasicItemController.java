a95548372d553287fea67590b0755862c4f3b64e
who-hoo <who.ho3ov@gmail.com>
2022-03-14 21:43:35 +0900

    :sparkles: 저장 후 리다이렉트 된 페이지에서 저장 완료 메시지 출력
    RedirectAttributes를 이용하여 저장 후 리다이렉트 된 페이지에서 저장 완료 메시지 출력

--- src/main/java/study/springitemservice/web/basic/BasicItemController.java
+++ src/main/java/study/springitemservice/web/basic/BasicItemController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import study.springitemservice.domain.item.Item;
 import study.springitemservice.domain.item.ItemRepository;
 
@@ -73,10 +74,18 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
+    //    @PostMapping("/add")
     public String addItemV5(Item item) {
         itemRepository.save(item);
         return "redirect:/basic/items/" + item.getId();
+    }
+
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
